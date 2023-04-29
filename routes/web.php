76d3fa08b6d48f45ddf3655e1b0381318f4ad69c
lckish <92448931+lckish@users.noreply.github.com>
2023-04-29 16:05:54 +0400

    add adminLTE

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,5 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', App\Http\Controllers\HomeController::class)->name('home');
+Route::get('/admin',\App\Http\Controllers\Main\IndexController::class)->name('main.index');
