63b750189b951ce5a2eaedf325c764d677e8d4d9
lckish <92448931+lckish@users.noreply.github.com>
2023-05-03 14:42:45 +0400

    add App.vue an assets, change app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
 import './bootstrap';
+
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+// createApp(App).mount('#app')
+
+//допсал сам
+import router from './router'
+import axios from "axios";
+
+const app = createApp(App)
+
+app.use(router)
+app.config.globalProperties.axios = axios
+app.mount('#app')
+
