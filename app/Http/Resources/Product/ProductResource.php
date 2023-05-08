5a6971425099b9ce56df6eea67d0a3c96c76a01e
Шкляев Кирилл Д19-191-1 <92448931+lckish@users.noreply.github.com>
2023-05-08 04:31:34 +0400

    add work frontend - fix old_price          backend -

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -23,7 +23,7 @@ class ProductResource extends JsonResource
             'content' => $this->content,
             'image_url' => $this->imageUrl,
             'price' => $this->price,
-            'old_price' => $this->price,
+            'old_price' => $this->old_price,
             'count' => $this->count,
             'is_published' => $this->is_published,
             'category'=> new CategoryResource($this->category)
--- resources/js/views/product/Index.vue
+++ resources/js/views/product/Index.vue
@@ -187,55 +187,6 @@
                         </div>
                         <div class="col-xl-9 col-lg-8">
                             <div class="row">
-                                <div class="col-xl-12">
-                                    <div
-                                        class="shop-grid-page-top-info p-0 justify-content-md-between justify-content-center">
-                                        <div class="left-box wow fadeInUp animated">
-                                            <p>Showing 1–12 of 50 Results</p>
-                                        </div>
-                                        <div
-                                            class="right-box justify-content-md-between justify-content-center wow fadeInUp animated">
-                                            <div class="short-by">
-                                                <div class="select-box">
-                                                    <select class="wide">
-                                                        <option data-display="Short by latest">Featured</option>
-                                                        <option value="1">Best selling</option>
-                                                        <option value="2">Alphabetically, A-Z</option>
-                                                        <option value="3">Alphabetically, Z-A</option>
-                                                        <option value="3">Price, low to high</option>
-                                                        <option value="3">Price, high to low</option>
-                                                        <option value="3">Date, old to new</option>
-                                                    </select>
-                                                </div>
-                                            </div>
-                                            <div
-                                                class="product-view-style d-flex justify-content-md-between justify-content-center">
-                                                <ul class="nav nav-pills" id="pills-tab" role="tablist">
-                                                    <li class="nav-item" role="presentation">
-                                                        <button class="nav-link active" id="pills-grid-tab"
-                                                                data-bs-toggle="pill" data-bs-target="#pills-grid"
-                                                                type="button"
-                                                                role="tab" aria-selected="true">
-                                                            <i class="flaticon-grid"></i>
-                                                        </button>
-                                                    </li>
-                                                    <li class="nav-item" role="presentation">
-                                                        <button class="nav-link" id="pills-list-tab"
-                                                                data-bs-toggle="pill"
-                                                                data-bs-target="#pills-list" type="button" role="tab"
-                                                                aria-selected="false">
-                                                            <i class="flaticon-list"></i>
-                                                        </button>
-                                                    </li>
-                                                </ul>
-                                                <button class="slidebarfilter d-lg-none d-flex"><i
-                                                    class="flaticon-edit"></i></button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
                                 <div class="col-12">
                                     <div class="tab-content" id="pills-tabContent">
                                         <div class="tab-pane fade show active" id="pills-grid" role="tabpanel"
@@ -384,9 +335,7 @@
                                                                                         <span class="increaseQty"> <i
                                                                                             class="flaticon-plus"></i>
                                                                                     </span></div>
-                                                                                    <button class="btn--primary "> Add
-                                                                                        to
-                                                                                        Cart
+                                                                                    <button class="btn--primary "> Добавить в корзину
                                                                                     </button>
                                                                                 </div>
                                                                             </div>
@@ -410,7 +359,9 @@
                                                         <div class="products-three-single-content text-center"> <span>{{ product.category.title }}</span>
                                                             <h5><a href="shop-details-3.html"> {{product.title}} </a>
                                                             </h5>
-                                                            <p>₽{{ product.price }}
+                                                            <p>
+                                                                <del>₽{{ product.old_price}}</del>
+                                                                ₽{{ product.price }}
                                                             </p>
                                                         </div>
                                                     </div>
